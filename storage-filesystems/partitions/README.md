614e25035b94d4bf2f5ab114452a3205554f61f7
kdefsys <236939850+kdefsys@users.noreply.github.com>
2026-02-03 03:30:47 +0000

    Agregué un nuevo script al directorio partitions/ con su respectiva actualizacion en el README.md

--- storage-filesystems/partitions/README.md
+++ storage-filesystems/partitions/README.md
@@ -3,7 +3,7 @@
 ## **Contenido**
 
 - [auditoria_disk_partitions.sh](#auditoria_disk_partitionssh)
-
+- [partition-risk-check.sh](#partition-risk-checksh)
 ____________________________________________________________________________________________________________________
 
 ## **auditoria_disk_partitions.sh**
@@ -45,3 +45,100 @@ ________________________________________________________________________________
 
    - usamos el lsblk -o pttype para el tipo de tabla de particiones, pero era mejor usar 'parted -s/dev/sdX print'
    - Se usa arrays asociativosen Bash para contar particiones por disco, evitando múltiples llamadas a comandos externos
+
+____________________________________________________________________________________________________________________"
+
+## **partition-risk-check.sh**
+   Nivel: Avanzado
+   **Temas:** Particiones de discos, tabla MBR/GPT, filesystems, auditoria preventiva, diseño de layout, lsblk,
+   bash scripting 
+
+   **Descripción Técnica**
+
+   Script de auditoria avanzada que analiza el estado del particionado y el diseño lógico del almacenamiento en
+   sistemas linux.
+
+   Actuá como un **pre-fligth check** antes de realizar operaciones críticas como:
+
+   - Ampliación de discos
+   - Modificaciones sobre LVM
+   - Creación o montaje de nuevos sistemas de archivos
+   - Migraciones de datos
+
+   El script evalúa 3 capas fundamentales:
+
+   1. **Riesgo estructural**
+	- Detecta discos sin particiones o con una sola partición
+	- Identifica discos con tabla MBR que pueden causar problemas en discos grandes.
+	
+   2. **Riesgo operacional**
+	- Detecta particiones sin filesystems
+	- Detecta particiones con filesystems no montadas.
+
+   3. **Diseño crítico del sistema de archivos**
+	- Evalúa si '/' y '/var' comparten la misma partición
+	- Verifica si '/boot' está correctamente separado
+
+   Clasifica los hallazgos en **OK**, **WARNING**, **CRITICAL**, y genera un reporte estructurado para toma de
+   deceisiones.
+
+   **Uso Típico en las empresas**
+
+   - Servidores Linux en producción
+   - Ambientes críticos (bases de datos, servidores web, sistemas de monitoreo).
+   - Auditorías de infraestructura
+   - Procedimiento de mantenimiento programado.
+   - Validaciones previas a cambios mayores en almacenamiento.
+
+   Permite detectar **errores de diseño historico** que suelen pasar desapercibidos hasta que ocurre una caída del
+   sistema
+
+   **Ejemplo de Ejecución**
+
+   sudo ./partition-risk-check.sh /var/log
+
+   **Curiosidad Técnica**
+
+   - Utiliza el lsblk con opciones -nro para obtener salidas limpias y controladas.
+   - Emplea arregos asociativos en Bash para contabilizar particiones por disco.
+   - Usa sustitucion de procesos para evitar subshells y conservar el estado de variables.
+
+   **Ejemplo de Salida**
+
+   [WARNING] disk sdb
+
+   Partition table	:mbr
+
+   Tamaño del disco     :3.6T
+
+   Mensaje		El disco es MBR y sobrepasa el limite de 2TB
+
+   Recomendacion        Convertir la tabal a gpt
+
+   [CRITICAL] partition sdb1
+
+   FILESYSTEM		:NO PRESENTE
+
+   MOUNTPOINT		:
+
+   Mensaje		:La particion no tiene un filesystem creado.
+
+   Recomendacion	:Crearle un filesystem a este archivo
+
+   [WARNING] Diseño del sistema de archivos
+
+   - / and /var comparten la misma particion sda2
+   - Riesgo: El crecimiento del registro puede llenar el sistema de archivo raiz
+
+   [SUMMARY]
+
+   Total de discos analizados: 2
+
+   Total de particiones analizadas: 5
+
+   OK: 3
+   WARNING: 2
+   CRITICAL: 1
+
+   ESTADO DEL SISTEMA: CRITICAL
+   Exit code 2
---
+++ storage-filesystems/partitions/partition-risk-check.sh
@@ -0,0 +1,266 @@
+#!/bin/bash
+# partition-risk-check.sh
+### Script que detecta riesgos de particionado que pueden causar probblemas graves en servidores de producción
+### Este script actuá como un pre-flight check antes de:
+### Ampliar discos, tocar LVM, montar nuevos filesystems y realizar migraciones
+
+function mensaje {
+	echo "$1"
+	echo "Saliendo del script..."
+	sleep 1
+}
+
+## El script debe si o sí ejecutarse con privilegios de superusuario
+
+if [[ "$EUID" -ne 0 ]]; then
+	$(mensaje "No fue ejecutado con privilegio de supuerusuario")
+	exit 1
+fi
+
+## Debe tener si o śi un parámetro de entrada
+
+if [[ "$#" -ne 1 ]]; then
+	$(mensaje "El script no tiene un parámetro de entrada")
+	exit 1
+fi
+
+## Recolecctamos los parámetros de entrada y declaramos variables
+
+DIR="$1"
+
+if [[ ! -d "$DIR" ]]; then
+	echo "El directorio no existe, desea crearlo? (Y/N): "
+	read -n1 op
+	case "$op" in
+		Y|y)
+		   mkdir log
+		   echo "Creado con éxito"
+		   ;;
+		N|n)
+		   $(mensaje "No se creó ningun directorio") ;;
+		*)
+		   $(mensaje "Opción incorrecta") ;;
+	esac
+fi
+
+FECHA=$(date +'%Y%m%d_%H%M')
+SALIDA="${DIR}/partition_disk_check_${FECHA}.log"
+
+cantidad_discos=0
+cantidad_particiones=0
+cantidad_ok=0
+cantidad_warning=0
+cantidad_critico=0
+mayor=0
+
+declare -A discos
+
+## Apuntamos cuantas particiones tiene un disco
+
+while read padre; do
+	if [[ -n "$padre" ]]; then
+		((++discos["$padre"]))
+	fi
+done < <(lsblk -nro pkname)
+
+exec 1> "$SALIDA"
+
+echo "====================================================="
+echo "PARTITION RISK CHECK REPORT"
+echo "====================================================="
+echo "Hostname: $(hostname)"
+echo "Date: $FECHA"
+echo "User: $USER"
+echo
+echo "-----------------------------------------------------"
+echo "[RIESGO ESTRUCTURAL]"
+echo "-----------------------------------------------------"
+echo
+echo
+
+## Primero veamos la parte de structural risks
+
+while read nombre tipo tama tipotable; do
+	if [[ "$tipo" != "disk" ]]; then
+		continue; fi
+	((++cantidad_discos))
+	estado="OK"
+	recomendacion="Ninguna, todo está muy bien en este disco"
+	if [[ -z "${discos[$nombre]}" ]]; then
+		estado="WARNING"
+		mensaje="El disco no tiene particiones"
+		recomendacion="Añadir particiones para este disco"
+		((++cantidad_warning))
+		if (( mayor < 1 )); then mayor=1; fi
+	elif [[ "${discos[$nombre]}" -eq 1 ]]; then
+		estado="WARNING"
+		mensaje="El disco solo tiene una partición"
+		recomendacion="Añadir mas particiones, no solo una"
+		((++cantidad_warning))
+		if (( mayor < 1 )); then mayor=1; fi
+	else
+		mensaje="Tiene varias particiones y sus tablas de particiones estan OK"
+	fi
+	if [[ "$tipotable" == "mbr" || "$tipotable" == "MBR" ]]; then
+		if [[ "$tama" == "2TB" || "$tama" == "2tb" ]]; then
+			((++cantidad_critico))
+			estado="CRITICAL"
+			mensaje="El disco es MBR y sobrepasa el límite de 2TB"
+			recomendacion="Convertir la tabla a gpt"
+			if (( mayor < 2 )); then mayor=2; fi
+		fi
+	fi
+	if [[ "$estado" == "OK" ]]; then
+		((++cantidad_ok))
+	fi
+	echo -e "[$estado] disk $nombre"
+	echo -e "\tPartition Table\t\t:${tipotable}"
+	echo -e "\tTamaño del disco\t:${tama}"
+	echo -e "\tMensaje\t\t:${mensaje}"
+	echo -e "\tRecomendación\t\t:${recomendacion}"
+	echo
+	echo
+
+done < <(lsblk -nro name,type,size,pttype)
+
+## Ahora veamos la zona de las particiones, si tienen filesystems o no, si están montadas o no
+
+echo "-----------------------------------------------------"
+echo "RIESGO OPERACIONAL"
+echo "-----------------------------------------------------"
+echo
+echo
+
+while read nombre tipo tama tipofs montaje; do
+	if [[ "$tipo" != "part" ]]; then
+		continue; fi
+	((++cantidad_particiones))
+	estado="OK"
+	mensaje="La particion tiene su filesystem creada y esta montada"
+	recomendacion="Ninguna, todo esta OK"
+	if [[ -n "$tipofs" ]]; then
+		filesystem="$tipofs"
+		if [[ -z "$montaje" ]]; then
+			((++cantidad_warning))
+			montado="NO MONTADO"
+			estado="WARNING"
+			mensaje="La partición tiene un filesystem creado pero no está montado"
+			recomendacion="Montar el sistema de archivo de esa particion"
+			if (( mayor < 1 )); then mayor=1; fi
+		else
+			montado="$montaje"
+		fi
+	else
+		((++cantidad_critico))
+		filesystem="NO PRESENTE"
+		estado="CRITICO"
+		mensaje="La partición no tiene un filesystem creado."
+		recomendacion="Crearle un filesystem a este archivo"
+		if (( mayor < 2 )); then mayor=2; fi
+	fi
+	if [[ "$estado" == "OK" ]]; then
+		((++cantidad_ok))
+	fi
+	echo -e "[$estado] Partiton: $nombre"
+	echo -e "\t- Filesystem\t:${filesystem}"
+	echo -e "\t- Mountpoint\t:${montado}"
+	echo -e "\t- Mensaje\t:${mensaje}"
+	echo -e "\t- Recomendacion\t:${recomendacion}"
+	echo
+	echo
+
+done < <(lsblk -nro name,type,size,fstype,mountpoint)
+
+case "$mayor" in
+	0)
+	  estado_sistema="OK" ;;
+	1)
+	  estado_sistema="WARNING" ;;
+	2)
+	  estado_sistema="CRITICAL" ;;
+esac
+
+## Ahora la parte de analisis del sistema de archivo
+
+echo "----------------------------------------------------------"
+echo "[DISEÑO DEL SISTEMA DE ARCHIVOS CRÍTICOS]"
+echo "----------------------------------------------------------"
+echo
+echo
+
+root_dev=""
+var_dev=""
+boot_dev=""
+
+## Recolectamos dispositivos por mountpoint
+
+while read nombre montaje; do
+	case "$montaje" in
+		"/")
+		   root_dev="$nombre" ;;
+		"/var")
+		   var_dev="$nombre" ;;
+		"/boot")
+		   boot_dev="$nombre" ;;
+	esac
+done < <(lsblk -nro name,mountpoint)
+
+layout_ok=1
+
+## / y /var en la misma particion
+
+if [[ -n "$root_dev" && -n "$var_dev" && "$root_dev" == "var_dev" ]]; then
+	echo "[WARNING] Diseño del sistema de archivos"
+	echo " - / and /var comparten la misma particion $(root_dev)"
+	echo " - Riesgo: El crecimiento del registro puede llenar el sistema de archivo raiz"
+	echo
+	((++cantidad_warning))
+	layout_ok=0
+	if (( mayor < 1 )); then mayor=1; fi
+fi
+
+## /boot no separado
+
+if [[ -z "$boot_dev" ]]; then
+	echo "[WARNING] /boot"
+	echo " - Problema: No está en una partición separada"
+	echo " - Impacto: Riesgo de actualización del kernel"
+	echo
+	((++cantidad_warning))
+	layout_ok=0
+	if (( mayor < 1 )); then mayor=1; fi
+fi
+
+## layout sano
+
+if (( layout_ok == 1 )); then
+	echo "[OK] Diseño de sistema de archivos"
+	echo " -/, /var and /boot debidamente separados"
+	echo
+	(( ++cantidad_ok))
+fi
+
+## Ahora ponemos los resultados totales
+
+echo "--------------------------------------------------"
+echo "[SUMMARY]"
+echo "--------------------------------------------------"
+echo
+echo "Total de discos analizados ${cantidad_discos}"
+echo "Total de particiones analizadas ${cantidad_particiones}"
+echo
+echo "OK: ${cantidad_ok}"
+echo "WARNING: ${cantidad_warning}"
+echo "CRITICAL: ${cantidad_critico}"
+echo
+echo "--------------------------------------------------"
+echo "RESULTADO FINAL"
+echo "--------------------------------------------------"
+echo
+echo "ESTADO DEL SISTEMA : $estado_sistema"
+echo "Exit code: $mayor"
+echo
+echo "--------------------------------------------------"
+echo "FIN DEL REPORTE"
+echo "--------------------------------------------------"
+

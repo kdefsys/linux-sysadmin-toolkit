eb624754d5e1521d559ba06553a82fcc84529b74
kdefsys <236939850+kdefsys@users.noreply.github.com>
2026-04-19 18:06:04 +0000

    Modificamos el script limpieza_contenido_logs.sh para que sea mas robusto

--- daily-tools/limpieza_contenido_logs.sh
+++ daily-tools/limpieza_contenido_logs.sh
@@ -4,15 +4,31 @@
 
 # Revisamos un directorio de logs y listamos todos los archivos de .log
 
-DIRECTORIO="$1"
-LISTA_LOGS=$(find $DIRECTORIO -type f -name "*.log")
+FECHA=$(date '+%Y-%m-%d %H:%M:%S')
+DIRECTORIO="${1:-.}"
+
+## Verificamos si el directorio existe"
+
+if [[ ! -d "$DIRECTORIO" ]]; then
+	echo "El directorio no existe"
+	echo "Procedemos a salir del script"
+	sleep 3
+	exit 1
+fi
+
+mapfile -t LISTA_LOGS < <(find "$DIRECTORIO" -type f -name "*.log")
 SALIDA="log_limpio.txt"
 
-echo "=== Limpieza de logs ejecutada: $FECHA === " > "$OUTPUT"
+echo "=== Limpieza de logs ejecutada: $FECHA === " > "$SALIDA"
 
-for archivo in $LISTA_LOGS; do
-	sort "$archivo" | uniq >> "$SALIDA"
+for archivo in "${LISTA_LOGS[@]}"; do
+	if [[ -f "$archivo" ]]; then
+		sort "$archivo" | uniq >> "$SALIDA"
+	fi
 done
 
-echo -e "\n=== Top 10 entradas más frecuentes ==="
-sort "$SALIDA" | uniq -c | sort -nr | head -n 10
+echo -e "\n=== Top 10 entradas más frecuentes ===" >> "$SALIDA"
+sort "$SALIDA" | grep -v "===" | uniq -c | sort -nr | head -n 10 >> "$SALIDA"
+
+echo "Mostrando la salida en la terminal"
+cat "$SALIDA"

76f1daba0318e10192e1f95507a312927e89c32c
kdefsys <236939850+kdefsys@users.noreply.github.com>
2026-07-22 19:28:08 +0000

    refactor(limpieza): Mejoré el script llamado: limpieza_contenido_logs.sh

--- daily-tools/limpieza_contenido_logs.sh
+++ daily-tools/limpieza_contenido_logs.sh
@@ -1,35 +1,64 @@
 #!/bin/bash
-### Nombre: Limpieza_logs.sh
-### Autor: kdefsys
-### Limpieza de logs duplicados y resumen de entradas frecuentes
-### Revisamos un directorio de logs y listamos todos los archivos de .log
-### Uso: ./limpieza_contenido_logs.sh <directorio>
+###Nombre: limpieza_contenido_logs.sh
+###Autor: kdefsys
+###Descripción: Este script escanea un directorio en busca de archivos de registro (con extensión .log), procesa su contenido para eliminar las líneas duplicadas
+###consecutivas o repetidas y genera un reporte consolidando que incluya un análisis del top10 de entradas más frecuentes
+###Uso: ./limpieza_contenido_logs.sh -d <directorio> -h [help]
 
-FECHA=$(date '+%Y-%m-%d %H:%M:%S')
-DIRECTORIO="${1:-.}"
+OPERACION="NO"
 
-## Verificamos si el directorio existe"
+function help {
+	echo "Uso: $0 -d <directorio> [-h]"
+        echo "  -d: Directorio objetivo a buscar los archivos .log"
+        echo "  -h: Muestra esta ayuda"
+        exit 0
+}
 
-if [[ ! -d "$DIRECTORIO" ]]; then
-	echo "El directorio no existe"
-	echo "Procedemos a salir del script"
-	sleep 3
-	exit 1
-fi
 
-mapfile -t LISTA_LOGS < <(find "$DIRECTORIO" -type f -name "*.log")
-SALIDA="log_limpio.txt"
-
-echo "=== Limpieza de logs ejecutada: $FECHA === " > "$SALIDA"
-
-for archivo in "${LISTA_LOGS[@]}"; do
-	if [[ -f "$archivo" ]]; then
-		sort "$archivo" | uniq >> "$SALIDA"
-	fi
+while getopts :d:h opt; do
+	case "$opt" in
+		d)
+		  DIRECTORIO="$OPTARG"
+		  if [[ ! -d "$DIRECTORIO" ]]; then DIRECTORIO="$(pwd)"; fi
+		  OPERACION="SI"
+		  ;;
+		h)
+		  help
+		  ;;
+		*)
+		  echo "Opción Inválida"
+		  exit 1
+		  ;;
+	esac
 done
 
-echo -e "\n=== Top 10 entradas más frecuentes ===" >> "$SALIDA"
-sort "$SALIDA" | grep -v "===" | uniq -c | sort -nr | head -n 10 >> "$SALIDA"
+if [[ "$OPERACION" == "SI" ]]; then
+	REPORTE="log_limpio.txt"
+	FECHA=$(date '+%Y-%m-%d %H:%M:%S')
 
-echo "Mostrando la salida en la terminal"
-cat "$SALIDA"
+	exec 3>"$REPORTE"
+
+	echo "============Limpieza de logs ejecutada: $FECHA=========" >&3
+	mapfile -t archivos_logs < <(find "$DIRECTORIO" -type f -name "*.log")
+
+	if [[ "${#archivos_logs[@]}" -eq 0 ]]; then
+		echo "No hay ningun archivo .log dentro del directorio objetivo: $DIRECTORIO"
+		exec 3>&-
+		rm -f "$REPORTE"
+		exit 0
+	fi
+
+	##Ponemos todo el contenido de todos los archivos en un solo archivo
+	for archivo in "${archivos_logs[@]}"; do
+		[[ -f "$archivo" ]] && cat "$archivo" >&3
+	done
+	exec 3>&-
+	echo -e "\n====== Top 10 entradas más frecuentes ========" >> "$REPORTE"
+	grep -v "====" "$REPORTE" | sort | uniq -c | sort -k 1nr,1nr | head -n 10 >> "$REPORTE"
+
+	cat "$REPORTE"
+else
+	echo "No se introdujo la opcion -d"
+	echo "Ejecutar así: $0 -d <directorio>"
+	exit 1
+fi

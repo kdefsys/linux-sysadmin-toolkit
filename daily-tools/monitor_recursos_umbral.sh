004c32cbecf2e2d818a8b8732b9292cfdcb75a19
kdefsys <236939850+kdefsys@users.noreply.github.com>
2026-07-23 18:35:23 +0000

    refactor(calculo): Mejora del script monitor_recursos_umbral.sh

--- daily-tools/monitor_recursos_umbral.sh
+++ daily-tools/monitor_recursos_umbral.sh
@@ -1,36 +1,63 @@
 #!/bin/bash
 ### Nombre: monitor_recursos_umbral.sh
 ### Autor: kdefsys
-### Leemos el contenido del archivo monitor.conf que tiene restricciones umbrales
-### El script se encarga de verificar si el sistema excede o no esas restricciones
-### Si es asi, entonces identificamos que procesos son los culpables de esto
-### Y si no es asi, entonces mandamos una señal de todo OK
-### Uso: ./monitor_recursos_umbral.sh <archivo>
+### Descripción: El script lee umbrales de consumo desde un archivo de configuración externo, evalua el uso global actual de CPU y memoria RAM en el sistema
+### y en caso de superar los limites, registra una alerta, junto con los procesos que mas recursos estan consumiendo
+### Uso: ./monitor_recursos_umbral.sh <file>
 
-FECHA=$(date '+%Y-%m-%d %H:%M:%S')
-SALIDA="alertas_recursos.log"
-
-exec 3>>"$SALIDA"
-
-if [[ "$#" -eq 0 ]]; then
-	echo "No se ingreso ningun parametro"
+if [[ "$#" -ne 1 ]]; then
+	echo "Argumentos invalidos"
+	echo "Debe ejecutarse así: $0 <file>"
 	exit 1
 fi
 
-FILE="$1"
+RUTA_FILE_UMBRAL="$1"
+REPORTE="alertas_recursos.log"
+FECHA=$(date '+%Y-%m-%d %H-%M-%S')
 
-if [[ ! -f "$FILE" ]]; then
-	echo "El archivo no existe"
+
+if [[ ! -f "${RUTA_FILE_UMBRAL}" ]]; then
+	echo "El archivo ingresado no existe"
 	exit 1
 fi
 
-MAX_CPU=$(gawk '{print $1}' "$FILE")
-MAX_RAM=$(gawk '{print $2}' "$FILE")
+exec 3>>"$REPORTE"
+MAX_CPU="$(gawk '{print $1}' $RUTA_FILE_UMBRAL)"
+MAX_RAM="$(gawk '{print $2}' $RUTA_FILE_UMBRAL)"
 
-CPU_GLOBAL=$(top -bn1 | grep -i "cpu(s)" | gawk '{print $2 + $4}' | cut -d "." -f 1)
-RAM_GLOBAL=$(free | grep -i "MEm" | gawk '{print $3/$2 * 100.0}' | cut -d "." -f 1)
+##==============================================================================
+## CALCULAMOS LA CPU
+##==============================================================================
 
-echo "==========REPORTE: '$FECHA'==========" >&3
+read -r _ user1 nice1 system1 idle1 iowait1 irq1 softirq1 steal1 _ < /proc/stat
+total1=$((user1 + nice1 + system1 + idle1 + iowait1 + irq1 + softirq1 + steal1))
+idle1_total=$((idle1 + iowait1))
+
+sleep 1
+
+read -r _ user2 nice2 system2 idle2 iowait2 irq2 softirq2 steal2 _ < /proc/stat
+total2=$((user2 + nice2 +system2 + idle2 + iowait2 + irq2 + softirq2 + steal2))
+idle2_total=$((idle2 + iowait2))
+
+diff_total=$((total2 - total1))
+diff_idle=$((idle2_total - idle1_total))
+
+CPU_GLOBAL=$(( 100 * (diff_total - diff_idle)/ diff_total ))
+
+##==============================================================================
+## CÁLCULO DEL USO DE LA MEMORIA
+##==============================================================================
+
+RAM_TOTAL=$(grep MemTotal /proc/meminfo | gawk '{print $2}')
+RAM_AVAIL=$(grep MemAvailable /proc/meminfo | gawk '{print $2}')
+
+RAM_GLOBAL=$(( 100 * (RAM_TOTAL - RAM_AVAIL) / RAM_TOTAL ))
+
+##==============================================================================
+##				REPORTE
+##==============================================================================
+
+echo "================REPORTE: '$FECHA' ========================" >&3
 
 if (( CPU_GLOBAL >= MAX_CPU )); then
 	echo "[ALERTA CPU] Uso actual: $CPU_GLOBAL% (Limite: $MAX_CPU%)" >&3
@@ -39,10 +66,10 @@ if (( RAM_GLOBAL >= MAX_RAM )); then
 	echo "[ALERTA RAM] Uso actual: $RAM_GLOBAL% (Limite: $MAX_RAM%)" >&3
 fi
 
-if (( CPU_GLOBAL >= MAX_CPU || RAM_GLOBAL >= MAX_RAM)); then
-	ps -eo user,pid,pcpu,pmem,stat,cmd --no-headers | sort -k 3nr,4nr | head -n 5 >&3
+if (( CPU_GLOBAL >= MAX_CPU || RAM_GLOBAL >= MAX_RAM )); then
+	ps -eo user,pid,pcpu,pmem,stat,cmd --no-headers | sort -k3,3nr -k4,4nr | head -n 5 >&3
 else
-	echo "Todo Ok, no se excede ni en CPU ni en RAM" >&3
+	echo "Todo OK, no se excede ni en CPU ni en RAM" >&3
 fi
 
 exec 3>&-

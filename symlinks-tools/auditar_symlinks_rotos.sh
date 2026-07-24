d075f59aebf26e192631e5d5ae8d934fddfd7c25
kdefsys <236939850+kdefsys@users.noreply.github.com>
2026-07-24 17:06:08 +0000

    refactor(auditoria): Mejora al script auditar_symlinks_rotos.sh

--- symlinks-tools/auditar_symlinks_rotos.sh
+++ symlinks-tools/auditar_symlinks_rotos.sh
@@ -1,56 +1,85 @@
 #!/bin/bash
 ### Nombre: auditar_symlinks_rotos.sh
 ### Autor: kdefsys
-### Identifica y registra todos los enlaces simbólicos rotos dentro de un directorio
-### específico proporcionado por el usuario
-### Uso: ./auditar_symlinks_rotos.sh <directorio>
+### Descripción: Los enlaces simbólicos pueden quedar "huérfanos" o "rotos" cuando el archivo o directorio de destino es movido o eliminado
+### Este script escanea rutas, identifica estos enlaces dañados, genera un informe de registro (log) y permite limpiarlos de forma segura
+### Uso: ./auditar_symlinks_rotos.sh -d <directorio_a_examinar>  -f <ruta_destino_log> -h [help]
 
-if [[ "$#" -ne 1 ]]; then
-	echo -e "El script no tiene ningun argumento\nSaliendo del script..."
-	exit 1
-fi
+##=================================================================
+##		FUNCIÓN HELP
+##=================================================================
 
-DIRECTORIO="$1"
-FECHA=$(date '+%Y-%m-%d_%H-%M-%S')
+function help {
+	echo " GUIA"
+	echo "  -d : directorio base a examinar"
+	echo "  -f : ruta de destino del archivo .log"
+	echo "  -h : esta ayuda"
+}
 
-if [[ -d "$DIRECTORIO" ]]; then
+##=================================================================================
+##			MENU DE GETOPTS
+##=================================================================================
 
-	SALIDA="auditoria_enlaces_rotos_${FECHA}.log"
+OPERACION="NO"
+RUTA_DESTINO="$(pwd)"
 
-	exec 3>>"$SALIDA"
+while getopts :d:f:h opt; do
+	case "$opt" in
+		d)
+		 DIRECTORIO="$OPTARG"
+		 if [[ ! -d "$DIRECTORIO" ]]; then
+			DIRECTORIO="$(pwd)"
+		 fi
+		 OPERACION="SI"
+		 ;;
+		f)
+		 RUTA_DESTINO="$OPTARG"
+		 if [[ ! -d "$RUTA_DESTINO" ]]; then
+			RUTA_DESTINO="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+		 fi
+		 ;;
+		h)
+		 help
+		 exit 0
+		 ;;
+		*)
+		 echo "Opción de argumento inválida"
+		 help
+		 exit 1
+		 ;;
+	esac
+done
 
-	echo -e "=============================Enlaces rotos en ${DIRECTORIO}===============================" >&3
-
-	mapfile -t enlaces_rotos < <(find "$DIRECTORIO" -type l -not -exec test -e {} \; -print)
-
-	if [[ "${#enlaces_rotos[@]}" -eq 0 ]]; then
-		echo "No hay enlaces rotos" >&3
+if [[ "$OPERACION" == "SI" ]]; then
+	FECHA=$(date '+%Y-%m-%d_%H-%M-%S')
+	REPORTE="${RUTA_DESTINO}/enlaces_rotos_${FECHA}.log"
+	exec 3>>"$REPORTE"
+	echo -e "============================ REPORTE DE ENLACES ROTOS EN EL DIRECTORIO $DIRECTORIO ==================================" >&3
+	mapfile -t enlaces_rotos < <(find "$DIRECTORIO" -type l ! -exec test -e {} \; -print)
+	CANTIDAD_ENLACES_ROTOS="${#enlaces_rotos[@]}"
+	if [[ "$CANTIDAD_ENLACES_ROTOS" -eq 0 || -z "${enlaces_rotos[0]}" ]]; then
+		echo "No se encontraron enlaces rotos en el directorio: $DIRECTORIO" >&3
 	else
+		echo "Si se encontraron enlaces rotos en el directorio: $DIRECTORIO" >&1
+		echo "Lista de los enlaces rotos encontrados en $DIRECTORIO" >&3
 		for enlace in "${enlaces_rotos[@]}"; do
-			archivo_destino=$(readlink "$enlace")
-			printf "Ruta de enlace:%s\tArchivo Destino:%s\n" "${enlace}" "${archivo_destino}" >&3
+			echo "ENLACE: $enlace	RUTA: $(readlink -f "$enlace")" >&3
 		done
-		read -p "Desea eliminar estos enlaces? (s/n): " opcion
-		case "$opcion" in
-			s|S)
-				for enlace in "${enlaces_rotos[@]}"; do
-					if rm -iv "$enlace"; then
-						echo "Enlace: $enlace eliminado del sistema" >&3
-					fi
-				done ;;
-			*)
-				echo "No hay proceso de eliminación"
-				;;
-		esac
+		read -p "Desea eliminarlos (s/n): " op
+		if [[ "$op" == "s" || "$op" == "S" ]]; then
+			echo "Procedemos a eliminar los enlaces rotos del directorio $DIRECTORIO"
+			echo -e "Se activo el proceso de eliminarlos\nLista de eliminacion" >&3
+			printf "%s\n" "${enlaces_rotos[@]}" | xargs rm -fv >&3
+			echo "Proceso de eliminacion terminado..."
+		else
+			echo "Se cancelo la operacion de eliminar a los enlaces rotos del directorio $DIRECTORIO"
+		fi
 	fi
-
+	echo "EL REPORTE FINAL FUE: "
+	cat "$REPORTE"
+	exec 3>&-
 else
-	echo -e "El directorio ingresado como primer argumento no existe\nSaliendo del script..."
+	echo "No se puede realizar ninguna operación porque no introdujo el directorio a examinar"
+	help
 	exit 1
 fi
-
-echo -e "El contenido completo del archivo log resultante es:"
-
-cat "$SALIDA"
-
-exec 3>&-

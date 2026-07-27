8bd438392bd07c9a70c7f34f55901fc600c26d30
kdefsys <236939850+kdefsys@users.noreply.github.com>
2026-07-27 22:09:37 +0000

    refactor: Mejora en el script auditar_archivos_criticos.sh

--- shell-core-concepts/auditar_archivos_criticos.sh
+++ shell-core-concepts/auditar_archivos_criticos.sh
@@ -1,35 +1,54 @@
 #!/bin/bash
 ### Nombre: auditar_archivos_criticos.sh
 ### Autor: kdefsys
-### En servidores compartidos, es comun que archivos criticos de configuracion
-### o logs crezcan, cambien permisos o sean modificados fuera de horario laboral
-### Vamos a recibir un parametro asi que ejecutar asi:
-### Uso: ./auditar_archivos_criticos.sh <directorio>
+### Descripción: En un entorno de servidores compartidos, los administradores de sistemas necesitan mantener bajo control los cambios no autorizados o inesperados. Es habitual que
+### archivos de configuración o de registro (logs) crezcan de forma desmedida, cambien sus permisos o sean modificados fuera del horario laboral habitual.
+### Este script realiza una auditoria periodica de archivos criticos dentro de una ruta especificada
+### Se busca unicamente archivos regulares, que tengan extensiones tipicas de configuracion o logs ( .conf, .cnf o .log), que hayan sido modificados en las ultimas 24 horas
+### y ademas excluye cualquier archivo que se encuentre dentro de carpetas temporales (rutas que contengan /tmp)
+### Uso: ./auditar_archivos_criticos.sh -d <directorio> [-h]
 
-# Recibimos como parametro un directorio base
+function help {
+	echo "Este script realiza una auditoria periodica de archivos criticos dentro de una ruta especificada"
+	echo "Se busca unicamente archivos regulares, que tengan extensiones tipicas de configuracion o logs ( .conf, .cnf o .log), que hayan sido modificados en las ultimas 24 horas"
+	echo "y ademas excluye cualquier archivo que se encuentre dentro de carpetas temporales (rutas que contengan /tmp)"
+	echo "Se debe ejecutar: ./auditar_archivos_criticos.sh -d <directorio> [-h]"
+	echo "   -d : Directorio objetivo que se va a auditar, si no se introduce se asume que es el directorio actual"
+	echo "   -h : Imprime esta guia"
+}
 
-DIRECTORIO="${1:-.}"
-FECHA=$(date +'%Y-%m-%d')
-SALIDA="reporte_auditoria_$FECHA.log"
-
-if [[ ! -d "$DIRECTORIO" ]]; then
-	echo "El directorio introducido no existe"
-	echo "Saliendo del script"
-	exit 1
-fi
-
-mapfile -t archivos < <(find "$DIRECTORIO" -type f -not -path "*/tmp/*" \
-	\( -name "*.conf" -or -name "*.cnf" -or -name "*.log" \) -mtime -1 \
-	-exec stat -c "%n|%s|%y|%Y" {} \; | gawk -F "|" 'BEGIN{OFS="\t"}{ 
-		print $1, $2, $3
-	}')
-
-exec 3>>"$SALIDA"
-
-echo -e "RUTA\t\tTAMAÑO\t\tFECHA_MODIFICACION\n" >&3
-
-for lineas in "${archivos[@]}"; do
-	echo "$lineas" >&3
+DIRECTORIO="$(pwd)"
+while getopts :d:h opt; do
+	case "$opt" in
+		d)
+		 if [[ -d "$OPTARG" ]]; then
+			DIRECTORIO="$OPTARG";
+		 else
+			echo "El directorio $OPTARG no existe, saliendo del script ..." >&2
+			exit 1
+		 fi
+		 ;;
+		h)
+		 help
+		 exit 0
+		 ;;
+		*)
+		 echo "Opcion invalida"
+		 help
+		 exit 1
+		 ;;
+	esac
 done
 
+mapfile -t files < <(find "$DIRECTORIO" -type f ! -path "*/tmp/*" -regextype posix-extended -regex "(.*\.conf$|.*\.cnf$|.*\.log$)" -mtime -1 -exec stat -c "%s|%y|%n" {} \; \
+	| gawk -F "|" 'BEGIN{OFS="\t"} {print $1, $2, $3}')
+
+FECHA=$(date '+%Y-%m-%d_%H-%M-%S')
+REPORTE="reporte_auditoria_archivos_criticos_${FECHA}.log"
+exec 3>>"$REPORTE"
+
+echo -e "TAMAÑO\t\tFECHA_MODIFICACION\t\tRUTA\n" >&3
+printf "%s\n" "${files[@]}" >&3
+
+echo "Auditoria completada con exito. Reporte generado en: $REPORTE"
 exec 3>&-

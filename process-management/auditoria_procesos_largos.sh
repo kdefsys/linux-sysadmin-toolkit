eba86b742ac1bf579d62b477cb13e020e5a81a67
kdefsys <236939850+kdefsys@users.noreply.github.com>
2026-08-04 18:02:38 +0000

    refactor: Mejora en el script auditoria_procesos_largos.sh

--- process-management/auditoria_procesos_largos.sh
+++ process-management/auditoria_procesos_largos.sh
@@ -1,59 +1,102 @@
 #!/bin/bash
 ### Nombre: auditoria_procesos_largos.sh
 ### Autor: kdefsys
-### Identifica los procesos activos que superen un tiempo de ejecución determinado, filtrando procesos
-### del sistema y evaluando la severidad de la carga en el servidor
-### Uso: ./auditoria_procesos_largos.sh <directorio_log> <tiempo_maximo> <limite_tolerancia>
+### Descripcion: En la administración de servidores Linux, los procesos que permanecen activos en ejecución o en espera durante demasiado tiempo pueden representar fugas de recursos,
+### tareas colgadas o consumidores excesivos de CPU/Memoria. Sin embargo, no todos los procesos duraderos son un problema: el kernel del sistema mantiene hilos internos (kernel threads)
+### y servicios esenciales que operan de forma continua. Por ello, es necesario auditar el sistema filtrando esos hilos del kernel y monitorear únicamente los procesos de usuario/aplicación
+### que superen un tiempo límite configurado.
+### Este script inspecciona la tabla de procesos activos, identifica aquellos procesos que llevan ejecutandose mas de un tiempo determinado (convertido a minutos), aplica filtros para
+### descartar procesos del kernel y el propio script de auditoria, y genera un reporte detallado clasificando el nivel de severidad del servidor.
+### Uso: ./auditoria_procesos_largos.sh -d <directorio_log> -m <tiempo_maximo> -c <limite_tolerancia> [-h]
 
-if [[ "$#" -ne 3 ]]; then
-	echo -e "El script no recibió 3 argumentos\nSaliendo del script"
-	sleep 2
-	exit 1
-fi
+function help {
+	echo "El script debe de ejecutarse asi: ./auditoria_procesos_largos.sh -d <directorio_log> -m <tiempo_maximo> -s <limite_tolerancia> [-h]"
+	echo "   -d : Directorio donde se guardara el archivo de reporte. Si no se introduce se asume que es el directorio actual de trabajo"
+	echo "   -m : Tiempo maximo permitido en minutos."
+	echo "   -c : Limite de tolerancia (cantidad maxima de procesos largos tolerables antes de pasar a un estado de alerta critica"
+	echo "   -h : Imprime esta guia"
+}
 
-if [[ ! -d "$1" ]]; then
-	echo -e "El directorio ingresado como primer argumento no existe\nSaliendo del script..."
-	sleep 2
-	exit 1
-fi
+DIRECTORIO="$(pwd)"
+TIEMPO=""
+LIMITE=""
 
-DIRECTORIO="$1"
-TIEMPO=$(( $2 * 60 ))
-FECHA=$(date '+%Y-%m-%d_%H-%M-%S')
-LIMITE="$3"
-ESTADO="OK"
-REPORTE="${DIRECTORIO}/auditoria_procesos_largos_${HOSTNAME}_${FECHA}.log"
-SCRIPT_NAME=$(basename "$0")
+while getopts :d:m:c:h opt; do
+	case "$opt" in
+		d)
+		 DIRECTORIO="$OPTARG"
+		 if [[ ! -d "$DIRECTORIO" ]]; then
+			echo "El directorio ingresado no existe. Saliendo del script..." >&2
+			exit 1
+		 fi
+		 ;;
+		m)
+		 TIEMPO="$OPTARG"
+		 if ! [[ "$TIEMPO" =~ ^[0-9]+$ ]]; then
+			echo "El tiempo introducido no es un entero" >&2
+			exit 1
+		 fi
+		 ;;
+		c)
+		 LIMITE="$OPTARG"
+		 if ! [[ "$LIMITE" =~ ^[0-9]+$ ]]; then
+			echo "El limite introducido no es un entero" >&2
+			exit 1
+		 fi
+		 ;;
+		h)
+		 help
+		 exit 0
+		 ;;
+		*)
+		 echo "Opcion ingresada invalida" >&2
+		 help
+		 exit 1
+		 ;;
+	esac
+done
 
-exec 3>>"${REPORTE}"
-echo -e "===================================REPORTE DE PROCESOS LARGOS==================================\n" >&3
-echo -e "FECHA: ${FECHA}\nHOST: ${HOSTNAME}\nPARAMETROS USADOS: "$1" "$2" "$3"\n\n" >&3
+if [[ -n "$TIEMPO" && -n "$LIMITE" ]]; then
+	FECHA=$(date '+%Y-%m-%d_%H-%M-%S')
+	REPORTE="${DIRECTORIO}/reporteprocesoslargos_${HOSTNAME}_${FECHA}.log"
+	exec 3>>"$REPORTE"
+	echo "=========================================================== REPORTE DE PROCESOS LARGOS ====================================================" >&3
+	echo "ESTE REPORTE ES EL RESULTADO DEL SCRIPT: auditoria_procesos_largos.sh">&3
+	echo "HOST: ${HOSTNAME}" >&3
+	echo "TIEMPO LIMITE: $(( TIEMPO * 60 ))" >&3
+	echo "LIMITE DE CANTIDAD: $LIMITE" >&3
+	echo "===========================================================================================================================================" >&3
+	TIEMPO=$(( TIEMPO * 60 ))
+	SCRIPT="$(basename "$0")"
 
-mapfile -t procesos < <(ps -eo pid,ppid,uid,etimes,start,pcpu,pmem,stat,cmd --no-headers | while read -r pid ppid uid etimes inicio cpu mem estado comando; do
-    if [[ "$estado" =~ ^(S|R|D) ]]; then
-        if [[ "$etimes" -gt "$TIEMPO" ]]; then
-            # Filtro de kernel threads y evitar que el script se lea a sí mismo
-            if [[ ! "$comando" =~ ^\[.*\]$ && "$comando" != *"${SCRIPT_NAME}"* ]]; then
-                printf "PID: %s | PPID: %s | UID: %s | TIME: %ss | START: %s | CPU: %s%% | MEM: %s%% | STAT: %s | CMD: %s\n" \
-                        "$pid" "$ppid" "$uid" "$etimes" "$inicio" "$cpu" "$mem" "$estado" "$comando"
-            fi
-        fi
-    fi
-done)
+	mapfile -t procesos_largos < <(ps -eo pid,ppid,uid,etimes,start,pcpu,pmem,stat,cmd --no-headers \
+		| gawk -v t="$TIEMPO" -v script="$SCRIPT" 'BEGIN{OFS="|"} $8~/^(S|R|D)/ && $4>t && $9!~/^\[.*\]/ && $0!~script{ $9=""; for(i=9; i<=NF; i++) $9 = $9 (i==9 ? "" : " ") $i; print $1, $2, $3, $4, $5, $6, $7, $8, $9}')
 
-CANTIDAD=${#procesos[@]}
-
-if [[ "${#procesos[@]}" -eq 0 ]]; then
-	echo -e "No hay procesos con esas caracteristicas TODO OK\n" | tee -a "${REPORTE}"
-	exit 1
-else
-	echo -e "LISTA DE PROCESOS FILSTADOS:\n\n" >&3
-	printf "%s\n" "${procesos[@]}" >&3
-	if [[ "$CANTIDAD" -ge 1 && "$CANTIDAD" -le "$LIMITE" ]]; then
-		echo "CANTIDAD: ${CANTIDAD} ESTADO: OBSERVACION" >&3
-	else echo "CANTIDAD: ${CANTIDAD} ESTADO: ALERTA OPERATIVA" >&3
+	CANTIDAD="${#procesos_largos[@]}"
+	if [[ "$CANTIDAD" -eq 1 && -z "${procesos_largos[0]}" ]]; then
+		CANTIDAD=0
 	fi
-fi
+	if (( CANTIDAD == 0 )); then
+		echo "No se encontraron procesos largos con esas caracteristicas" >&3
+	else
+		echo "Se encontraron $CANTIDAD procesos largos con esas caracteristicas" >&3
+		echo "Estos son: " >&3
+		echo -e "PID\t\tPPID\t\tUID\t\tTIME\t\tSTART\t\tPCPU\t\tPMEM\t\tSTAT\t\tCMD\n" >&3
+		printf "%s\n" "${procesos_largos[@]}" | gawk -F "|" 'BEGIN{OFS="\t"}{print $1, $2, $3, $4, $5, $6, $7, $8, $9}' >&3
+	fi
+	echo "========================================================== LEYENDA =================================================================" >&3
+	echo "====================================================================================================================================" >&3
 
-echo -e "Termino el proceso, los datos estan guardados en ${REPORTE}"
-exec 3>&-
+	if (( CANTIDAD == 0 )); then
+		echo "ESTADO GENERAL DEL SISTEMA: OK" >&3
+	elif (( CANTIDAD <= LIMITE ));then
+		echo "ESTADO GENERAL DEL SISTEMA: OBSERVACION" >&3
+	else
+		echo "ESTADO GENERAL DEL SISTEMA: ALERTA OPERATIVA" >&3
+	fi
+
+	exec 3>&-
+	echo "auditoria_procesos_largos.sh concluyo con exito. Ver el reporte en: $REPORTE"
+else
+	echo "No se puede ejecutar el script porque no tiene los dos argumentos obligatorios que son el tiempo y el limite." >&2
+fi

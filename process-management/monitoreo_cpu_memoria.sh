5d7cc7edb1b10f8f62fbd15e1bf5bcc87ffb75f0
kdefsys <236939850+kdefsys@users.noreply.github.com>
2026-08-04 22:30:38 +0000

    refactor: Mejora en el script monitoreo_cpu_memoria.sh

--- process-management/monitoreo_cpu_memoria.sh
+++ process-management/monitoreo_cpu_memoria.sh
@@ -1,81 +1,134 @@
 #!/bin/bash
 ### Nombre: monitoreo_cpu_memoria.sh
 ### Autor: kdefsys
-### Audtia los procesos del sistema, detecta aquellos que exceden umbrales de consumo definidos por el usuario
-### y genera un reporte con una clasificacion de severidad dinamica.
+### Descripcion: En entornos de producción, el consumo desmedido de recursos por parte de un solo proceso puede desestabilizar el servidor completo. Para prevenir caídas de servicio,
+### el equipo de operaciones requiere una herramienta de auditoría automatizada que analice la tabla de procesos en tiempo real, identifique aquellos que sobrepasen umbrales de uso
+### de CPU o Memoria RAM predefinidos, y los clasifique dinámicamente según la severidad del exceso.
+### Este script recibe parametros de tolerancia de recursos (CPU y Memoria), audita los procesos activos en el sistema descartando la propia ejecucion de la auditoria, evala la
+### severidad del consumo de cada proceso mediante el uso de calculo con decimales, suma el impacto total de los procesos excedidos y guarda los hallazgos ordenados en un reporte de
+### salida.
+### Uso: ./monitoreo_cpu_memoria.sh [-d <directorio_salida>] -m <umbral_memoria> -p <umbral_cpu> [-h]
 
-if [[ "$#" -ne 3 ]]; then
-	echo -e "El script no recibe 3 argumentos\nSaliendo del script"
-	sleep 2
-	exit 1
-fi
+function help {
+	echo "El script se debe de ejecutar asi: ./monitoreo_cpu_memoria.sh [-d <directorio_salida>] -m <umbral_memoria> -p <umbral_cpu> [-h]"
+	echo "   -d : Directorio donde se va guardar el reporte. Si no se introduce la bandera, se asumira que es el directorio actual de trabajo."
+	echo "   -m : Umbral maximo de memoria RAM permitido (porcentaje)."
+	echo "   -p : Umbral maximo de CPU permitido (porcentaje)."
+	echo "   -h : Imprime esta guia."
+}
 
-if [[ ! -d "$1" ]]; then
-	echo -e "El directorio ingresado como primer argumento no existe\nSaliendo del script..."
-	sleep 2
-	exit 1
-fi
+DIRECTORIO="$(pwd)"
+UMBRAL_CPU=""
+UMBRAL_MEM=""
 
-FECHA=$(date '+%Y-%m-%d_%H-%M-%S')
-REPORTE="${1}/cpu_mem_monitor_${HOSTNAME}_${FECHA}.log"
-SUMA_CPU=0
-SUMA_MEM=0
-NAME_SCRIPT=$(basename "$0")
+while getopts :d:m:p:h opt; do
+	case "$opt" in
+		d)
+		 DIRECTORIO="$OPTARG"
+		 if [[ ! -d "$DIRECTORIO" ]]; then
+			echo "El directorio ingresado no existe. Saliendo del script." >&2
+			exit 1
+		 fi
+		 ;;
+		m)
+		 UMBRAL_MEM="$OPTARG"
+		 if ! [[ "$UMBRAL_MEM" =~ ^[0-9]+$ ]]; then
+			echo "El valor de UMBRAL DE MEMORIA no es un numero entero. Saliendo del script." >&2
+			exit 1
+		 fi
+		 if ! (( UMBRAL_MEM >= 0 && UMBRAL_MEM <= 100 )); then
+			echo "El valor de UMBRAL DE MEMORIA RAM no es correcto para un porcentaje." >&2
+			exit 1
+		 fi
+		 ;;
+		p)
+		 UMBRAL_CPU="$OPTARG"
+		 if ! [[ "$UMBRAL_CPU" =~ ^[0-9]+$ ]]; then
+			echo "El valor de UMBRAL DE CPU no es un numero entero. Saliendo del script." >&2
+			exit 1
+		 fi
+		 if ! (( UMBRAL_CPU >= 0 && UMBRAL_CPU <= 100 )); then
+		 	echo "El valor de UMBRAL DE CPU no es correcto para un porcentaje." >&2
+			exit 1
+		 fi
+		 ;;
+		h)
+		 help
+		 exit 0
+		 ;;
+		*)
+		 echo "Opcion ingresada invalida" >&2
+		 help
+		 exit 1
+		 ;;
+	esac
+done
 
-if [[ "$2" -ge 0 && "$2" -le 100 && "$3" -ge 0 && "$3" -le 100 ]]; then
-	UMBRAL_CPU="$2"
-	UMBRAL_MEM="$3"
-else
-	echo -e "No es aceptable esos numeros\nSaliendo del script..."
-	sleep 2
-	exit 1
-fi
-
-function detectando {
-	local valor="$1"
+function evaluacion {
+	local consumo="$1"
 	local umbral="$2"
-	local advertencia=$(echo "$umbral * 1.5" | bc -l)
-	if (( $(echo "$valor <= $umbral" | bc -l) )); then
+	# Evaluamos mediante bc devolviendo 1 (true) o 0 (false)
+	local es_ok=$(echo "$consumo <= $umbral" | bc -l)
+	local es_obs=$(echo "$consumo <= ($umbral * 1.5)" | bc -l)
+
+	if [[ "$es_ok" -eq 1 ]]; then
 		echo "OK"
-	elif (( $(echo "$valor <= $advertencia" | bc -l) )); then
+	elif [[ "$es_obs" -eq 1 ]]; then
 		echo "OBSERVACION"
 	else
 		echo "CRITICO"
 	fi
 }
 
-mapfile -t procesos < <(ps -eo pid,ppid,pcpu,pmem,etimes,cmd --no-headers | while read -r pid ppid pcpu pmem etime cmd; do
-		if [[ "$cmd" == *"${NAME_SCRIPT}"* ]]; then
-			continue
-		fi
-		ESTADO_CPU=$(detectando "$pcpu" "$UMBRAL_CPU")
-		ESTADO_MEM=$(detectando "$pmem" "$UMBRAL_MEM")
-		if [[ "$ESTADO_CPU" != "OK" || "$ESTADO_MEM" != "OK" ]]; then
-			printf "PID: %s | PPID: %s | CPU: %s | MEM: %s | TIME_TRANS: %s | COMANDO: %s | ESTADO_CPU: %s | ESTADO_MEM: %s\n" \
-				"$pid" "$ppid" "$pcpu" "$pmem" "$etime" "$cmd" "$ESTADO_CPU" "$ESTADO_MEM"
-		fi
-	done)
+if [[ -n "$UMBRAL_CPU" && -n "$UMBRAL_MEM" ]]; then
+	FECHA=$(date '+%Y-%m-%d_%H-%M-%S')
+	REPORTE="${DIRECTORIO}/cpu_mem_monitor_${HOSTNAME}_${FECHA}.log"
+	SCRIPT="$(basename "$0")"
+	exec 3>>"$REPORTE"
 
-TOTALES=$(printf "%s\n" "${procesos[@]}" | gawk -F "|" '
-	{
-		split($3, cpu, " "); split($4, mem, " ");
-		s_cpu += cpu[2] ; s_mem += mem[2]
-	}
-	END{print s_cpu " " s_mem}')
+	echo "=========================================================== REPORTE DE UMBRAL DE CPU Y MEM ====================================================" >&3
+	echo "ESTE REPORTE HA SIDO GENERADO POR EL SCRIPT: monitoreo_cpu_memoria.sh" >&3
+	echo "HOSTNAME: $HOSTNAME" >&3
+	echo "FECHA: $FECHA" >&3
+	echo "UMBRAL DE CPU: $UMBRAL_CPU" >&3
+	echo "UMBRAL DE MEMORIA RAM: $UMBRAL_MEM" >&3
+	echo "===============================================================================================================================================" >&3
 
-read SUMA_CPU SUMA_MEM <<< "$TOTALES"
+	mapfile -t procesos < <(ps -eo pid,ppid,pcpu,pmem,etimes,cmd --no-headers \
+		| gawk -v script="$SCRIPT" 'BEGIN{OFS="|"} $0!~script{$6=""; for(i=6;i<=NF;++i) $6 = $6 (i==6 ? "" : " ") $i; print $1, $2, $3, $4, $5, $6}' \
+		| while IFS="|" read -r pid ppid pcpu pmem etimes cmd; do
+			estado_cpu=$(evaluacion "$pcpu" "$UMBRAL_CPU")
+			estado_mem=$(evaluacion "$pmem" "$UMBRAL_MEM")
+			if [[ "$estado_cpu" != "OK" || "$estado_mem" != "OK" ]]; then
+				echo "$pid|$ppid|$pcpu|$pmem|$etimes|$cmd|$estado_cpu|$estado_mem"
+			fi
+		done)
 
-exec 3>>"${REPORTE}"
+	CANTIDAD="${#procesos[@]}"
+	if [[ "$CANTIDAD" -eq 1 && -z "${procesos[0]}" ]]; then
+		CANTIDAD=0
+	fi
+	if (( CANTIDAD == 0 )); then
+		echo "No se encontraron procesos que superen dichos umbrales" >&3
+	else
+		CONSUMO_TOTAL_CPU=0
+		CONSUMO_TOTAL_MEM=0
 
-echo -e "==============================MONITOREO DE PROCESOS CON PCPU Y PMEM EXCEDIDOS AL UMBRAL=================================" >&3
-echo -e "LISTA DE PROCESOS:\n\n" >&3
-if [[ "${#procesos[@]}" -gt 0 ]]; then
-	printf "%s\n" "${procesos[@]}" | sort -t "|" -k 3nr,3nr -k 4nr,4nr | tee -a "${REPORTE}" &> /dev/null
+		CONSUMO_TOTAL_CPU=$(printf "%s\n" "${procesos[@]}" | gawk -F "|" '{s+=$3} END{print s}')
+		CONSUMO_TOTAL_MEM=$(printf "%s\n" "${procesos[@]}" | gawk -F "|" '{s+=$4} END{print s}')
+
+		echo "LOS PROCESOS ENCONTRADOS SON: ">&3
+		echo -e "PID\t\tPPID\t\tPCPU\t\tPMEM\t\tETIMES\t\tCMD\t\tESTADO_CPU\t\tESTADO_MEM\n" >&3
+		printf "%s\n" "${procesos[@]}" | LC_ALL=C sort -t "|" -k 3,3nr -k 4,4nr | gawk -F "|" 'BEGIN{OFS="\t"}{print $1, $2, $3, $4, $5, $6, $7, $8}' >&3
+
+		echo "=================================================================== LEYENDA ===============================================================" >&3
+		echo "TOTAL DE CPU CONSUMIDA: $CONSUMO_TOTAL_CPU" >&3
+		echo "TOTAL DE MEMORIA CONSUMIDA: $CONSUMO_TOTAL_MEM" >&3
+		echo "===========================================================================================================================================" >&3
+	fi
+
+	exec 3>&-
+	echo "monitoreo_cpu_memoria.sh concluido correctamente. Puede ver el reporte en: $REPORTE"
 else
-	echo "No se encontraron procesos que excedan esos umbrales" >&3
+	echo "No se introdujeron las banderas obligatorias (-m y -p). Saliendo del script."
 fi
-printf "PCPU TOTAL %s\nPMEM TOTAL: %s\n" "${SUMA_CPU}" "${SUMA_MEM}" >&3
-
-exec 3>&-
-
-echo "Proceso Finalizado, Reporte en ${REPORTE}"
